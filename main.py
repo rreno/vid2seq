fa0d16f8606fcabe64fa1af6511353b46d379cc3
Ryan Reno <ryan.reno@outlook.com>
2018-04-28 14:43:58 -0700

    Added basic documentation

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ DEFAULT_OUT_TEXT = 'OUTPUT'
 
 
 class MainWindow(QMainWindow):
+    '''
+    MainWindow class
+    Inherits from PySide2.QtWidgets.QMainWindow
+    
+    Loads ui file, connects signals and slots to appropriate children widgets
+    and handles the logic.
+
+    TODO: Seperate concerns. Manage UI elements and logic in seperate clsases
+    '''
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -20,7 +29,10 @@ class MainWindow(QMainWindow):
         self.window = self.loader.load(self.ui)
         self.centralWidget = self.window.centralWidget()
         self.menuExit = self.window.menuBar().children()[0]
-
+        
+        '''
+        Get all of the children of the centralWidget and assign them by name
+        '''
         self.children = {}
         for child in self.centralWidget.children()[0].children():
             self.children.update({child.objectName(): child})
@@ -31,6 +43,10 @@ class MainWindow(QMainWindow):
         self.lblOut = self.children['lblOut']
         self.btnGo = self.children['btnGo']
 
+        '''
+        Set up file path and video name defaults and disable execution of
+        ffmpeg until after video and output location have been selected
+        '''
         self.inPath = './'
         self.outPath = './'
         self.vidName = ''
@@ -47,17 +63,25 @@ class MainWindow(QMainWindow):
         self.window.show()
 
     def selectInputVideo(self):
+        '''
+        Opens a dialog to allow the user to select a video. Saves the full path to later pass to ffmpeg
+        '''
         video = QFileDialog.getOpenFileName(parent=self.centralWidget,
                                             caption='Input Video',
                                             dir=self.inPath)
+        # video is a tuple of (filename, selectedFilter). We just need the first element
         self.inPath = QDir(video[0]).absolutePath()
         self.lblIn.setText(self.inPath)
+        # remove the path and the video container information so we can name our sequence files later.
         self.videoName = self.inPath.split('/')[-1].split('.')[0]
 
         if self.lblIn.text() != DEFAULT_IN_TEXT and self.lblOut.text() != DEFAULT_OUT_TEXT:
             self.btnGo.setEnabled(True)
 
     def selectOutputDir(self):
+        '''
+        Opens a dialog so the user can select an output folder for the sequence.
+        '''
         out = QFileDialog.getExistingDirectory(parent=self.centralWidget,
                                                caption='Output Directory',
                                                dir=self.outPath)
@@ -68,6 +92,9 @@ class MainWindow(QMainWindow):
             self.btnGo.setEnabled(True)
 
     def convertVideo(self):
+        '''
+        Calls ffmpeg with the input and output paths.
+        '''
         print("Converting")
         try:
             subprocess.run("ffmpeg -i {} {}/{}_%03d.png".format(self.inPath,
